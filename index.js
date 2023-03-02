409493e3856c3f02daa6cc090fc589777582373b
bernieshan <bernie.shan01@icloud.com>
2023-03-01 19:41:35 -0800

    Added rest of pt. 5 files

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ export function allButtons() {
 }
 
 function submitFunc() {
-	var fname = document.getElementById("firstname").value;
-	var lname = document.getElementById("lastname").value;
-	var email = document.getElementById("email").value;
-	var position = document.getElementById("position").value;
-	var hearabout = document.getElementById("hearabout").value;
-	var message = document.getElementById("message").value;
+	var fname = DOMPurify.sanitize(document.getElementById("firstname").value);
+	var lname = DOMPurify.sanitize(document.getElementById("lastname").value);
+	var email = DOMPurify.sanitize(document.getElementById("email").value);
+	var position = DOMPurify.sanitize(document.getElementById("position").value);
+	var hearabout = DOMPurify.sanitize(document.getElementById("hearabout").value);
+	var message = DOMPurify.sanitize(document.getElementById("message").value);
 	window.alert("Your Response:\nFirst Name: " + fname
 		+ "\nLast Name: " + lname + "\nEmail: " + email +
 		"\nPosition: " + position + "\nHow did you hear about me?: " +
