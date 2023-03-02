2012fe30b8c6c3d20989cc214c0a91f85c235c58
bernieshan <bernie.shan01@icloud.com>
2023-03-01 19:29:00 -0800

    Added Pt 5 content

---
+++ index.js
@@ -0,0 +1,17 @@
+export function allButtons() {
+	var submit = document.getElementById("submitform");
+	submit.addEventListener("click", submitFunc);
+}
+
+function submitFunc() {
+	var fname = document.getElementById("firstname").value;
+	var lname = document.getElementById("lastname").value;
+	var email = document.getElementById("email").value;
+	var position = document.getElementById("position").value;
+	var hearabout = document.getElementById("hearabout").value;
+	var message = document.getElementById("message").value;
+	window.alert("Your Response:\\nFirst Name: " + fname
+		+ "\\nLast Name: " + lname + "\\nEmail: " + email +
+		"\\nPosition: " + position + "\\nHow did you hear about me?: " +
+		hearabout + "\\nYour Message: " + message);
+}
